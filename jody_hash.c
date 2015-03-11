1303141c445bc2ab29bef7ee63eee621f743d381
Jody Bruchon <jody@c02ware.com>
2015-03-11 13:11:10 -0400

    Change jody_hash license to MIT

--- jody_hash.c
+++ jody_hash.c
@@ -6,11 +6,7 @@
  * and faster.
  *
  * Copyright (C) 2014-2015 by Jody Bruchon <jody@jodybruchon.com>
- * Released under the terms of the GNU GPL version 2
- *
- * 2015-01-03: Re-licensed under the fdupes license as detailed in the
- * README file. Though no longer required, I would still request that
- * anyone who improves this code send me patches! Thanks. -Jody
+ * Released under the MIT License (see LICENSE for details)
  */
 
 #include <stdio.h>
