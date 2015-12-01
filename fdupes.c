683aec4390d3398babfcf26ed29d2d346327c94c
Jody Bruchon <jody@c02ware.com>
2015-12-01 15:31:32 -0500

    Toss redundant getfilestats() calls, add more debug stats

--- fdupes.c
+++ fdupes.c
@@ -132,7 +132,8 @@ typedef struct _filetree {
 
 /* Hash/compare performance statistics */
 static unsigned int small_file = 0, partial_hash = 0, partial_to_full = 0, hash_fail = 0;
-static uintmax_t filecount = 0;
+static uintmax_t filecount = 0, comparisons = 0, left_branch = 0, right_branch = 0;
+static unsigned int tree_depth = 0, max_depth = 0;
 
 /* Directory parameter position counter */
 static unsigned int user_dir_count = 1;
@@ -580,7 +581,6 @@ static hash_t *getcrcsignatureuntil(file_t * const checkfile,
   FILE *file;
 
   /* Get the file size. If we can't read it, bail out early */
-  getfilestats(checkfile);
   if (checkfile->size == -1) return NULL;
   fsize = checkfile->size;
 
@@ -649,8 +649,6 @@ static inline void purgetree(filetree_t *checktree)
 
 static inline void registerfile(filetree_t ** restrict branch, file_t * restrict file)
 {
-  getfilestats(file);
-
   *branch = (filetree_t*)string_malloc(sizeof(filetree_t));
   if (*branch == NULL) errormsg(NULL);
 
@@ -673,7 +671,8 @@ static file_t **checkmatch(filetree_t * const restrict checktree,
    * unintentionally. We don't want to flag these files as
    * duplicates unless the user specifies otherwise. */
 
-  getfilestats(file);
+  /* Count the total number of comparisons requested */
+  comparisons++;
 
 /* If considering hard linked files as duplicates, they are
  * automatically duplicates without being read further since
@@ -768,21 +767,31 @@ static file_t **checkmatch(filetree_t * const restrict checktree,
 
   if (cmpresult < 0) {
     if (checktree->left != NULL) {
+      left_branch++;
+      tree_depth++;
       return checkmatch(checktree->left, file);
     } else {
       registerfile(&(checktree->left), file);
+      if (max_depth < tree_depth) max_depth = tree_depth;
+      tree_depth = 0;
       return NULL;
     }
   } else if (cmpresult > 0) {
     if (checktree->right != NULL) {
+      right_branch++;
+      tree_depth++;
       return checkmatch(checktree->right, file);
     } else {
       registerfile(&(checktree->right), file);
+      if (max_depth < tree_depth) max_depth = tree_depth;
+      tree_depth = 0;
       return NULL;
     }
   } else {
     /* All compares matched */
     partial_to_full++;
+    if (max_depth < tree_depth) max_depth = tree_depth;
+    tree_depth = 0;
     return &checktree->file;
   }
   /* Fall through - should never be reached */
@@ -1704,6 +1713,10 @@ skip_full_check:
     fprintf(stderr, "\n%d partial (+%d small) -> %d full (%d partial elim) (%d hash fail)\n",
 		partial_hash, small_file, partial_to_full,
 		(partial_hash - partial_to_full), hash_fail);
+    fprintf(stderr, "%ju total files, %ju comparisons, branch L %ju, R %ju, both %ju\n",
+		    filecount, comparisons, left_branch, right_branch,
+		    left_branch + right_branch);
+    fprintf(stderr, "Maximum tree depth reached: %u\n", max_depth);
   }
 
   exit(0);
