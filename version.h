52d4cff8a94c90d934e48c86d2aab72fd6841c4f
Jody Bruchon <jody@c02ware.com>
2016-08-04 22:04:54 -0400

    v1.3: update documentation

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #ifndef JDUPES_VERSION_H
 #define JDUPES_VERSION_H
 
-#define VER "1.2.1"
-#define VERDATE "2016-04-04"
+#define VER "1.3"
+#define VERDATE "2016-08-04"
 
 #endif /* JDUPES_VERSION_H */
