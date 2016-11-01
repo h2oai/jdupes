2b7fc3d503f6d9ac96c09aae57937d68874bbbdb
Jody Bruchon <jody@c02ware.com>
2016-11-01 01:03:46 -0400

    v1.5.1: update CHANGES and README

--- version.h
+++ version.h
@@ -3,7 +3,7 @@
 #ifndef JDUPES_VERSION_H
 #define JDUPES_VERSION_H
 
-#define VER "1.5"
-#define VERDATE "2016-09-26"
+#define VER "1.5.1"
+#define VERDATE "2016-11-01"
 
 #endif /* JDUPES_VERSION_H */
