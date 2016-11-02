a0d51e6229158997a7c97e1745a28b28ae1b5c40
Jody Bruchon <jody@c02ware.com>
2016-11-02 00:07:43 -0400

    Do not allow hard linked files to pass through BTRFS dedupe

--- jdupes.c
+++ jdupes.c
@@ -1392,7 +1392,6 @@ void dedupefiles(file_t * restrict files)
 
   /* Main dedupe loop */
   while (files) {
-    LOUD(fprintf(stderr, "progress: file %d: hasdupes %d, size %jd\n", cur_file, files->hasdupes, (intmax_t)files->size);)
     if (files->hasdupes && files->size) {
       cur_file++;
       if (!ISFLAG(flags, F_HIDEPROGRESS)) {
@@ -1404,6 +1403,13 @@ void dedupefiles(file_t * restrict files)
       cur_info = 0;
       for (curfile = files->duplicates; curfile; curfile = curfile->duplicates) {
         int errno2;
+
+        /* Never allow hard links to be passed to dedupe */
+        if (curfile->device == files->device && curfile->inode == files->inode) {
+          LOUD(fprintf(stderr, "skipping hard linked file pair: '%s' = '%s'\n", curfile->d_name, files->d_name);)
+          continue;
+	}
+
         dupe_filenames[cur_info] = curfile->d_name;
         fd = open(curfile->d_name, O_RDWR);
         LOUD(fprintf(stderr, "opening loop: open('%s', O_RDWR) [%d]\n", curfile->d_name, fd);)
@@ -1454,7 +1460,8 @@ void dedupefiles(file_t * restrict files)
       for (cur_info = 0; cur_info < n_dupes; cur_info++) {
         status = same->info[cur_info].status;
         if (status != 0) {
-          fprintf(stderr, "couldn't dedupe %s => %s: %s [%d]\n", files->d_name,
+          fprintf(stderr, "warning: dedupe only did %jd byes: %s => %s: %s [%d]\n",
+            (intmax_t)same->info[cur_info].bytes_deduped, files->d_name,
             dupe_filenames[cur_info], dedupeerrstr(status), status);
         }
       }
