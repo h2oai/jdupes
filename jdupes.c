d825221d37c3d807d519d19d69da2e0ba95fa362
Jody Bruchon <jody@jodybruchon.com>
2017-12-04 02:32:50 -0500

    Remove currently unused spec in -X help text

--- jdupes.c
+++ jdupes.c
@@ -1632,7 +1632,7 @@ static inline void help_text(void)
   printf(" -x --xsize=SIZE  \texclude files of size < SIZE bytes from consideration\n");
   printf("    --xsize=+SIZE \t'+' specified before SIZE, exclude size > SIZE\n");
   printf(" -X --exclude=spec:info\texclude files based on specified criteria\n");
-  printf("                  \tspecs: dir size+-=\n");
+  printf("                  \tspecs: size+-=\n");
   printf("                  \tExclusions are cumulative: -X dir:abc -X dir:efg\n");
   printf(" -z --zeromatch   \tconsider zero-length files to be duplicates\n");
   printf(" -Z --softabort   \tIf the user aborts (i.e. CTRL-C) act on matches so far\n");
