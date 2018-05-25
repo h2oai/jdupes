2e78662b6722233c3575ecfa8776c4586af25650
Jody Bruchon <jody@jodybruchon.com>
2018-05-24 20:04:18 -0400

    v1.10.2 release

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #ifndef JDUPES_VERSION_H
 #define JDUPES_VERSION_H
 
-#define VER "1.10.1"
-#define VERDATE "2018-04-24"
+#define VER "1.10.2"
+#define VERDATE "2018-05-24"
 
 #endif /* JDUPES_VERSION_H */
