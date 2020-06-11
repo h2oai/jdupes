074f2cfa75dc70b18b997c3ae0b04b3b5bec287d
Jody Bruchon <jody@jodybruchon.com>
2020-06-10 21:53:56 -0400

    Silence two build warnings in win_stat.c

--- win_stat.c
+++ win_stat.c
@@ -25,7 +25,7 @@ static time_t nttime_to_unixtime(const uint64_t * const restrict timestamp)
 	newstamp /= 10000000LL;
 	if (newstamp <= 11644473600LL) return 0;
 	newstamp -= 11644473600LL;
-	return newstamp;
+	return (time_t)newstamp;
 }
 
 /* Get stat()-like extra information for a file on Windows */
@@ -52,7 +52,7 @@ int win_stat(const char * const filename, struct winstat * const restrict buf)
   if (!GetFileInformationByHandle(hFile, &bhfi)) goto failure2;
 
   buf->st_ino = ((uint64_t)(bhfi.nFileIndexHigh) << 32) + (uint64_t)bhfi.nFileIndexLow;
-  buf->st_size = ((uint64_t)(bhfi.nFileSizeHigh) << 32) + (uint64_t)bhfi.nFileSizeLow;
+  buf->st_size = ((int64_t)(bhfi.nFileSizeHigh) << 32) + (int64_t)bhfi.nFileSizeLow;
   timetemp = ((uint64_t)(bhfi.ftCreationTime.dwHighDateTime) << 32) + bhfi.ftCreationTime.dwLowDateTime;
   buf->st_ctime = nttime_to_unixtime(&timetemp);
   timetemp = ((uint64_t)(bhfi.ftLastWriteTime.dwHighDateTime) << 32) + bhfi.ftLastWriteTime.dwLowDateTime;
