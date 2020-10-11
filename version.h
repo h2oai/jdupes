18d567303a2d2713c38957c19cbb4de0881bcbb8
Jody Bruchon <jody@jodybruchon.com>
2020-10-11 10:56:48 -0400

    v1.19.0 release

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #ifndef JDUPES_VERSION_H
 #define JDUPES_VERSION_H
 
-#define VER "1.18.2"
-#define VERDATE "2020-07-20"
+#define VER "1.19.0"
+#define VERDATE "2020-10-11"
 
 #endif /* JDUPES_VERSION_H */
