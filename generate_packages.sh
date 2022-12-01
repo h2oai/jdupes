94891957fc2c239404051554f220f15bf7d0ce19
Jody Bruchon <jody@jodybruchon.com>
2022-12-01 15:40:50 -0500

    Add "bare bones" Make option and executable packaging
    make BARE_BONES=1
    
    will produce a jdupes executable with as much thrown out of the
    executable as possible. It is the same as LOW_MEMORY except with
    cuts that could be considered too aggressive, such as no deletion
    and no double-traversal protection.

--- generate_packages.sh
+++ generate_packages.sh
@@ -60,26 +60,29 @@ echo "Generating package for: $PKGNAME"
 mkdir -p "$PKGNAME"
 test ! -d "$PKGNAME" && echo "Can't create directory for package" && exit 1
 cp CHANGES README.md LICENSE $PKGNAME/
-E1=1; E2=1; E3=1
+E1=1; E2=1; E3=1; E4=1
 make clean && make -j$PM ENABLE_DEDUPE=1 stripped && cp $NAME$EXT $PKGNAME/$NAME$EXT && E1=0
 make clean && make -j$PM ENABLE_DEDUPE=1 LOUD=1 stripped && cp $NAME$EXT $PKGNAME/${NAME}-loud$EXT && E2=0
 make clean && make -j$PM LOW_MEMORY=1 stripped && cp $NAME$EXT $PKGNAME/${NAME}-lowmem$EXT && E3=0
+make clean && make -j$PM BARE_BONES=1 stripped && cp $NAME$EXT $PKGNAME/${NAME}-barebones$EXT && E4=0
 make clean
-test $((E1 + E2 + E3)) -gt 0 && echo "Error building packages; aborting." && exit 1
+test $((E1 + E2 + E3 + E4)) -gt 0 && echo "Error building packages; aborting." && exit 1
 # Make a fat binary on macOS x86_64 if possible
 if [ "$TA" = "mac64" ] && ld -v 2>&1 | grep -q 'archs:.*i386'
 	then
-	E1=1; E2=1; E3=1
+	E1=1; E2=1; E3=1; E4=1
 	BITS=32
 	make clean && make -j$PM CFLAGS_EXTRA=-m32 stripped && cp $NAME$EXT $PKGNAME/$NAME$EXT$BITS && E1=0
 	make clean && make -j$PM CFLAGS_EXTRA=-m32 LOUD=1 stripped && cp $NAME$EXT $PKGNAME/$NAME-loud$EXT$BITS && E2=0
 	make clean && make -j$PM CFLAGS_EXTRA=-m32 LOW_MEMORY=1 stripped && cp $NAME$EXT $PKGNAME/${NAME}-lowmem${EXT}$BITS && E3=0
+	make clean && make -j$PM CFLAGS_EXTRA=-m32 BARE_BONES=1 stripped && cp $NAME$EXT $PKGNAME/${NAME}-barebones${EXT}$BITS && E4=0
 	make clean
-	test $((E1 + E2 + E3)) -gt 0 && echo "Error building packages; aborting." && exit 1
+	test $((E1 + E2 + E3 + E4)) -gt 0 && echo "Error building packages; aborting." && exit 1
 	test "$E1" = "0" && lipo -create -output $PKGNAME/jdupes_temp $PKGNAME/$NAME$EXT$BITS $PKGNAME/$NAME$EXT && mv $PKGNAME/jdupes_temp $PKGNAME/$NAME$EXT
 	test "$E2" = "0" && lipo -create -output $PKGNAME/jdupes_temp $PKGNAME/$NAME-loud$EXT$BITS $PKGNAME/$NAME-loud$EXT && mv $PKGNAME/jdupes_temp $PKGNAME/$NAME-loud$EXT
 	test "$E3" = "0" && lipo -create -output $PKGNAME/jdupes_temp $PKGNAME/$NAME-lowmem$EXT$BITS $PKGNAME/$NAME-lowmem$EXT && mv $PKGNAME/jdupes_temp $PKGNAME/$NAME-lowmem$EXT
-	rm -f $PKGNAME/$NAME$EXT$BITS $PKGNAME/$NAME-loud$EXT$BITS $PKGNAME/$NAME-lowmem$EXT$BITS
+	test "$E4" = "0" && lipo -create -output $PKGNAME/jdupes_temp $PKGNAME/$NAME-barebones$EXT$BITS $PKGNAME/$NAME-barebones$EXT && mv $PKGNAME/jdupes_temp $PKGNAME/$NAME-barebones$EXT
+	rm -f $PKGNAME/$NAME$EXT$BITS $PKGNAME/$NAME-loud$EXT$BITS $PKGNAME/$NAME-lowmem$EXT$BITS $PKGNAME/$NAME-barebones$EXT$BITS
 fi
 test "$PKGTYPE" = "zip" && zip -9r $PKGNAME.zip $PKGNAME/
 test "$PKGTYPE" = "gz"  && tar -c $PKGNAME/ | gzip -9 > $PKGNAME.pkg.tar.gz
