1c9dbbf25f882512524c4a0d29e84c56eb353bbb
Jody Bruchon <jody@jodybruchon.com>
2023-06-19 15:38:58 -0400

    Add guards to likely_unlikely.h

--- likely_unlikely.h
+++ likely_unlikely.h
@@ -2,6 +2,13 @@
  * By Jody Bruchon <jody@jodybruchon.com>
  * Released to the public domain */
 
+#ifndef LIKELY_UNLIKELY_H
+#define LIKELY_UNLIKELY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* Un-define if already defined */
 #if !defined NO_LIKELY_UNLIKELY && (defined __GNUC__ || defined __clang__)
 #ifdef likely
@@ -18,3 +25,9 @@
 #define likely(a) a
 #define unlikely(a) a
 #endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIKELY_UNLIKELY_H */
