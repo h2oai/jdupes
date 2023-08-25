bd933f0506eb841064508882b18cdaca302fc42d
Jody Bruchon <jody@jodybruchon.com>
2023-08-25 02:56:40 -0400

    v1.27.0 release

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 #ifndef JDUPES_VERSION_H
 #define JDUPES_VERSION_H
 
-#define VER "1.26.1"
-#define VERDATE "2023-07-04"
+#define VER "1.27.0"
+#define VERDATE "2023-08-25"
 
 #endif /* JDUPES_VERSION_H */
