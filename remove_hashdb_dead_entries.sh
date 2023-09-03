9771861c4c6af0851a8972412e9bc000f055ef64
Jody Bruchon <jody@jodybruchon.com>
2023-09-03 13:34:10 -0400

    Improvements to hashdb cleaning script

--- remove_hashdb_dead_entries.sh
+++ remove_hashdb_dead_entries.sh
@@ -9,6 +9,14 @@ LINELEN=87
 
 [ "$HASHDB" = "." ] && HASHDB="jdupes_hashdb.txt"
 
+clean_exit () {
+	echo "Terminated, cleaning up." >&2
+	rm -f "$TEMPDB"
+	exit 1
+}
+
+trap clean_exit INT TERM HUP ABRT QUIT
+
 if ! grep -q -m 1 '^jdupes hashdb:2,' "$HASHDB"
 	then echo "Must be a version 2 database, exiting" >&2
 	exit 1
@@ -19,17 +27,18 @@ SRCLINES="$((SRCLINES - 1))"
 
 echo "Cleaning out hash database $HASHDB [$SRCLINES entries]" >&2
 
-while read LINE;
-	do if [ $CNT -eq 0 ]
-		then echo "$LINE" >> "$TEMPDB" || ERR=1
-		CNT=$((CNT + 1))
-		continue
-	fi
+head -n 1 "$HASHDB" > "$TEMPDB" || ERR=1
+
+echo "Sorting items (this may take a little time)..." >&2
+
+while read LINE
+	do
 	NAME="${LINE:$LINELEN}"
 	[ ! -e "$NAME" ] && echo "$LINE" >&2 && continue
 	echo "$LINE" >> "$TEMPDB" || ERR=1
 	CNT=$((CNT + 1))
-done < "$HASHDB"
+	echo -n "Processed $CNT/$SRCLINES lines ($((CNT * 100 / SRCLINES))%)"$'\r'
+done < <(grep -v '^jdupes hashdb:' "$HASHDB" | sort -k7 -t,)
 
 if [ $ERR -eq 1 ]
 	then echo "Error writing out lines, not overwriting hash database" >&2
@@ -37,7 +46,6 @@ if [ $ERR -eq 1 ]
 	exit 1
 
 	else
-	CNT=$((CNT - 1))  # Remove header line
 	mv -f "$TEMPDB" "$HASHDB"
 	echo "Wrote $CNT entries; cleaned out $((SRCLINES - CNT)) entries" >&2
 fi
